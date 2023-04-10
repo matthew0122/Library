b3df532e9994272744fb8102dd8e3df3e89101b5
Matthwe Bergin <matthewdbergin@gmail.com>
2023-04-10 17:13:27 -0400

    add books onto page

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 let myLibrary = [];
+const display = document.getElementById("display");
 const a = Book("hi", "hi", false, false, false, 0);
-console.log(a);
-
+//console.log(a);
+myLibrary.push(a);
 function Book(author, title, finished, inProgress, wantToRead, page) {
     return {
         author,
@@ -15,3 +16,29 @@ function Book(author, title, finished, inProgress, wantToRead, page) {
 function addBookToLibrary(author, title, finished, inProgress, wantToRead, page){
     myLibrary.push(Book(author, title, finished, inProgress, wantToRead, page));
 }
+function displayBooks(myLibrary){
+    for(let book of myLibrary){
+        const row = document.createElement('tr');
+        const author = document.createElement('td');
+        author.textContent = book.author;
+        const title = document.createElement('td');
+        title.textContent = book.title;
+        const finished = document.createElement('td');
+        finished.textContent = book.finished;
+        const inProgress = document.createElement('td');
+        inProgress.textContent = book.inProgress;
+        const wantToRead = document.createElement('td');
+        wantToRead.textContent = book.wantToRead;
+        const page = document.createElement('td');
+        page.textContent = book.page;
+        row.appendChild(author);
+        row.appendChild(title);
+        row.appendChild(finished);
+        row.appendChild(inProgress);
+        row.appendChild(wantToRead);
+        row.appendChild(page);
+        display.appendChild(row);
+    }
+    
+}
+displayBooks(myLibrary);
